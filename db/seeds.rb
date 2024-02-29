de223c57ebe46a342a73d3f8a83c7f7ae91732f2
Victor Guerra <victor.guerra@amlrightsource.com>
2024-02-29 17:49:15 +0000

    seeds: Improve seeds with promotion for each product

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,37 @@
 # frozen_string_literal: true
 
-# Create products
-products_to_create = [
-  {
-    name: 'Green Tea',
-    code: 'GR1',
-    price_cents: 311
-  },
-  {
-    name: 'Strawberries',
-    code: 'SR1',
-    price_cents: 500
-  },
-  {
-    name: 'Coffee',
-    code: 'CF1',
-    price_cents: 1123
-  }
-]
+green_tea = Product.find_or_create_by!(name: 'Green Tea', code: 'GR1', price_cents: 311)
+puts "Product #{green_tea.name} created"
+puts 'Creating promotion'
+Promotion.find_or_create_by!(
+  name: 'Buy one get one free',
+  discount: 100,
+  quantity: 2,
+  product: green_tea
+)
+puts "Promotion for '#{green_tea.name}' created"
+puts '===================='
 
-puts 'Creating products'
-Product.insert_all(products_to_create)
-puts 'Products created'
+strawberries = Product.find_or_create_by!(name: 'Strawberries', code: 'SR1', price_cents: 500)
+puts "Product #{strawberries.name} created"
+puts 'Creating promotion'
+Promotion.find_or_create_by!(
+  name: 'Buy 3 or more and get 10% off each strawberry',
+  discount: 50,
+  quantity: 3,
+  product: strawberries
+)
+puts "Promotion for '#{strawberries.name}' created"
+puts '===================='
+
+coffee = Product.find_or_create_by!(name: 'Coffee', code: 'CF1', price_cents: 1123)
+puts "Product #{coffee.name} created"
+puts 'Creating promotion'
+Promotion.find_or_create_by!(
+  name: 'Coffee addiction',
+  discount: 33,
+  quantity: 3,
+  product: coffee
+)
+puts "Promotion for '#{coffee.name}' created"
+puts '===================='
