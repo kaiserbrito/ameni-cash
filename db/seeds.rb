9113b176b509fd04185bd6795a08fcb41d2132f6
Victor Guerra <victor.guerra@amlrightsource.com>
2024-02-29 16:11:29 +0000

    seeds: Create products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,24 @@
 # frozen_string_literal: true
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+# Create products
+products_to_create = [
+  {
+    name: 'Green Tea',
+    code: 'GR1',
+    price_cents: 311
+  },
+  {
+    name: 'Strawberries',
+    code: 'SR1',
+    price_cents: 500
+  },
+  {
+    name: 'Coffee',
+    code: 'CF1',
+    price_cents: 1123
+  }
+]
+
+puts 'Creating products'
+Product.insert_all(products_to_create)
+puts 'Products created'
