a0e6c997ac391448c28ed90024333d077fe5f706
Victor Guerra <victor.guerra@amlrightsource.com>
2024-03-01 12:33:47 +0000

    feat: Improve model with validations and associations

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -10,4 +10,12 @@
 # cart_id     :bigint  not null
 #
 class CartProduct < ApplicationRecord
+  belongs_to :product
+  belongs_to :cart
+
+  validates :currency, :quantity, presence: true
+  validates :quantity, numericality: { greater_than: 0 }
+  validates :total_cents, numericality: { greater_than_or_equal_to: 0 }
+
+  monetize :total_cents, as: :total_price, with_model_currency: :currency
 end
--- spec/models/cart_product_spec.rb
+++ spec/models/cart_product_spec.rb
@@ -3,5 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe CartProduct, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'checks for presence of currency, quantity', :aggregate_failures do
+      is_expected.to validate_presence_of(:currency)
+      is_expected.to validate_presence_of(:quantity)
+
+      is_expected.to validate_numericality_of(:quantity).is_greater_than(0)
+      is_expected.to validate_numericality_of(:total_cents).is_greater_than_or_equal_to(0)
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to product and cart' do
+      is_expected.to belong_to(:product)
+      is_expected.to belong_to(:cart)
+    end
+  end
 end
