6c187fcd4762f71baa9e7f3aca1efd1d298ad67f
Victor Guerra <victor.guerra@amlrightsource.com>
2024-03-02 13:11:46 +0000

    feat: Improve views with Add to Cart button and a Cart side panel

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'products/index', type: :view do
+RSpec.describe 'products/index', type: :feature do
   let!(:green_tea) { create(:product, name: 'Green Tea', code: 'GR1', price_cents: 311) }
   let!(:strawberries) { create(:product, name: 'Strawberries', code: 'SR1', price_cents: 500) }
   let!(:coffee) { create(:product, name: 'Coffee', code: 'CF1', price_cents: 1123) }
@@ -35,17 +35,35 @@ RSpec.describe 'products/index', type: :view do
     )
   end
 
-  before { assign(:products, [green_tea, strawberries, coffee]) }
+  context 'when cart is empty' do
+    it 'renders a list of products with their promotions', :aggregate_failures do
+      visit products_path
 
-  it 'renders a list of products with their promotions', :aggregate_failures do
-    render
+      expect(page).to have_content('Products')
+      expect(page).to have_content('Green Tea')
+      expect(page).to have_content('Buy one get one free')
+      expect(page).to have_content('Strawberries')
+      expect(page).to have_content('Buy 3 or more and get 10% off each strawberry')
+      expect(page).to have_content('Coffee')
+      expect(page).to have_content('Coffee addiction')
 
-    expect(rendered).to match(/Products/)
-    expect(rendered).to match(/Green Tea/)
-    expect(rendered).to match(/Buy one get one free/)
-    expect(rendered).to match(/Strawberries/)
-    expect(rendered).to match(/Buy 3 or more and get 10% off each strawberry/)
-    expect(rendered).to match(/Coffee/)
-    expect(rendered).to match(/Coffee addiction/)
+      expect(page).to have_content('Cart')
+      expect(page).to have_content('Your cart is empty.')
+    end
+  end
+
+  context 'when add products to the cart' do
+    it 'updates cart total price', :aggregate_failures do
+      visit products_path
+
+      click_button('Add to Cart', match: :first)
+      expect(page).to have_content('Cart')
+      expect(page).to have_content('Green Tea 1')
+      expect(page).to have_content('Total: €3.11')
+
+      click_button('Add to Cart', match: :first)
+      expect(page).to have_content('Green Tea 2')
+      expect(page).to have_content('Total: €3.11')
+    end
   end
 end
